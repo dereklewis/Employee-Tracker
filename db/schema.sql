96801fec6617afdd71a4f89f92ea6160992d0e00
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-14 18:31:42 -0600

    server.js connections made, promise-mysql2 dependency added

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,23 @@
-DROP DATABASE IF EXISTS employee_db
-CREATE DATABASE employee_db
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employee_db
+USE employee_db;
 
 CREATE TABLE department (
     name VARCHAR(30)
-)
+);
 
 CREATE TABLE role (
-    id INT PRIMARY KEY
-    title VARCHAR(30)
-    salary DECIMAL
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT
-)
+);
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY
-    first_name VARCHAR(30)
-    last_name  VARCHAR(30)
-    role_id INT
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name  VARCHAR(30),
+    role_id INT,
     manager_id INT
-)
+);
